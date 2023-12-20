ccea82e3cb4796671d6923bed6b58d0fe8b1e35a
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2023-12-20 15:03:07 -0600

    03-countryApp: Adding Routes Component

---
+++ 03-countryApp/src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  templateUrl: './aboutPage.component.html',
+  styleUrl: './aboutPage.component.css',
+})
+export class AboutPageComponent { }
---
+++ 03-countryApp/src/app/shared/pages/homePage/homePage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  templateUrl: './homePage.component.html',
+  styleUrl: './homePage.component.css'
+})
+export class HomePageComponent { }

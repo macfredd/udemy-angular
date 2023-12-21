48ea277bd289514a5bc036aa005c5215c79f8130
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2023-12-20 19:17:30 -0600

    03-countryApp: Adding SideBarComponent

--- 03-countryApp/src/app/shared/shared.module.ts
+++ 03-countryApp/src/app/shared/shared.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/homePage/homePage.component';
 import { AboutPageComponent } from './pages/aboutPage/aboutPage.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [ 
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }

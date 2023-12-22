6d616b5a6551f2713df5481373331a1a98ef470c
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2023-12-22 17:29:52 -0600

    03-countryApp: Adding Table Component

--- 03-countryApp/src/app/countries/countries.module.ts
+++ 03-countryApp/src/app/countries/countries.module.ts
@@ -6,13 +6,15 @@ import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.com
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountriesRoutingModule } from './countires-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 @NgModule({
   declarations: [
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,

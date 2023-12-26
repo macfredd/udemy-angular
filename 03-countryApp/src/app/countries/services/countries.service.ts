a9a191b399ce4ee24b5ef79d473565c108b8e8cf
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2023-12-25 19:35:56 -0600

    03-countryApp: Adding RxJx High Func, extend countriesServices.

--- 03-countryApp/src/app/countries/services/countries.service.ts
+++ 03-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({
@@ -37,18 +37,31 @@ export class CountriesService {
    * @param term string
    * @returns Observable<Country>
    */
-    searchByRegion(term: string): Observable<Country[]> {
-      const url = `${this.apiUrl}/region/${term}`;
-      return this.searchCountries(term, url);
-    }
+  searchByRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.searchCountries(term, url);
+  }
 
-    /**
+  /**
+   * Get country by Alpha code
+   * @param id string
+   * @returns Observable<Country>
+   */
+  getCountryByAlpha(id: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.searchCountries(id, url)
+    .pipe(map( (countries:
+      Country[]) => countries.length > 0 ? countries[0] : null ))
+  }
+
+  /**
      * Search countries by Term and URL
      * @param term string
      * @param url string
      * @returns Observable<Country>
      */
-    private searchCountries(term: string, url: string): Observable<Country[]> {
-      return this.httpClient.get<Country[]>(url).pipe( catchError( (err) => of([]) ) );
-    }
+  private searchCountries(term: string, url: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(url).pipe( catchError( (err) => of([]) ) );
   }
+
+}

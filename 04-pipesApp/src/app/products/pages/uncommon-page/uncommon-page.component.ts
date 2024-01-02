0601df206743ee0104ad9bdba317fa78b763a48a
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-01-02 17:09:42 -0600

    04-pipesApp: Implementing i18nSelect Pipe

--- 04-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 04-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type gender = 'male' | 'female';
+export
+interface Client {
+  name: string;
+  gender: gender;
+}
 
 @Component({
   selector: 'app-uncommon-page',
@@ -7,4 +14,22 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18nSelect
+  public clients: Client[] = [
+    { name: 'Freddy', gender: 'male'},
+    { name: 'Adrea', gender: 'female'},
+    { name: 'Martin', gender: 'male'},
+    { name: 'Ana', gender: 'female'}
+  ];
+
+  public selectedClient: Client = this.clients[0];
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  };
+
+  public changeClient(): void {
+    this.selectedClient = this.clients[Math.floor(Math.random() * this.clients.length)];
+  }
 }

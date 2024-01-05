08c9982f14405488a0e98321beb4d385980889b1
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-01-05 11:44:21 -0600

    05-herosApp: Initial APP Setting, adding basic Routings and adding the components shell

---
+++ 05-herosApp/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+
+//localhost:4200
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'heros',
+    loadChildren: () => import('./heros/heros.module').then(m => m.HerosModule),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'heros',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+   }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

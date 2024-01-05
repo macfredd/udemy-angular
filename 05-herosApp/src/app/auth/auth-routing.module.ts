840decc585ab79afa61d58a3a3a44202d7cd06bf
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-01-05 13:11:45 -0600

    05-herosApp: Adding Childs Routings

--- 05-herosApp/src/app/auth/auth-routing.module.ts
+++ 05-herosApp/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 // localhost:4200/auth
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent, 
+    children: [
+      // localhost:4200/auth/login
+      { path: 'login', component: LoginPageComponent},
+      // localhost:4200/auth/register
+      { path: 'register', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' }
+    ]
   }
 ];
 
--- 05-herosApp/src/app/heros/heros-routing.module.ts
+++ 05-herosApp/src/app/heros/heros-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 // localhost:4200/heros
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
+    children: [
+      // localhost:4200/heros/list
+      { path: 'list', component: ListPageComponent },
+      // localhost:4200/heros/create
+      { path: 'create', component: NewPageComponent },
+      // localhost:4200/heros/edit/1
+      { path: 'edit/:id', component: NewPageComponent},
+      // localhost:4200/heros/search
+      { path: 'search/', component: SearchPageComponent },
+      // localhost:4200/heros/1
+      { path: ':id', component: HeroPageComponent },      
+      { path: '**', redirectTo: 'list' }
+    ]
   }
 ];
 

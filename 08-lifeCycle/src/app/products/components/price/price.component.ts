5b123894a4e93efba6d4b2c9e64ab0b72daf8e00
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-01-24 08:47:53 -0600

    08-LigeCycle: Adding onChange and OnDestroy to child component

--- 08-lifeCycle/src/app/products/components/price/price.component.ts
+++ 08-lifeCycle/src/app/products/components/price/price.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-price',
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent  implements OnInit, OnChanges, OnDestroy {
+  
+  @Input() price: number = 0;
 
+  ngOnInit(): void {
+    console.log('    PriceComponent - ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('    PriceComponent - ngOnChanges');
+    console.log('    ' + JSON.stringify(changes));
+  }
+
+  ngOnDestroy(): void {
+    console.log('    PriceComponent - ngOnDestroy');
+  }
 }
--- 08-lifeCycle/src/app/products/pages/product/product.component.ts
+++ 08-lifeCycle/src/app/products/pages/product/product.component.ts
@@ -7,62 +7,45 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class ProductComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
   public isProductVisible: boolean = true;
-  
-  // Ocurre cuando Angular inicializa el componente
+  public currentPrice: number = 0;
+
   constructor() { }
   
-  // Responde cuando Angular establece o restablece propiedades 
-  // de entrada vinculadas a datos. El método recibe un objeto 
-  // SimpleChanges de los valores de propiedad actuales y anteriores.
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges');
+    console.log(JSON.stringify(changes));
   }
   
-  // Inicialice la directiva o el componente después de que 
-  // Angular muestre por primera vez las propiedades vinculadas 
-  // a datos y establezca las propiedades de entrada de la 
-  // directiva o el componente.
   ngOnInit(): void {
     console.log('ngOnInit');
   }
 
-  // Detecta y actúa sobre cambios que Angular no puede o no 
-  // quiere detectar por sí solo.
   ngDoCheck(): void {
-    console.log('ngDoCheck');
+    console.log('ProdcutComponent - ngDoCheck');
   }
 
-  // Responde después de que Angular proyecta contenido externo 
-  // en la vista del componente o en la vista en la que se 
-  // encuentra una directiva.
   ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
+    console.log('ProdcutComponent - ngAfterContentInit');
   }
 
-  // Responde después de que Angular verifica el contenido 
-  // proyectado en la directiva o componente.
   ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
+    console.log('ProdcutComponent - ngAfterContentChecked');
   }
   
-  // Responde después de que Angular inicializa las vistas 
-  // y las vistas secundarias del componente, o la vista 
-  // que contiene la directiva.
   ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
+    console.log('ProdcutComponent - ngAfterViewInit');
   }
 
-  // Responde después de que Angular verifica las vistas 
-  // y las vistas secundarias del componente, o la vista 
-  // que contiene la directiva.
   ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
+    console.log('ProdcutComponent - ngAfterViewChecked');
   }
 
-  // Limpiezas justo antes de que Angular destruya la directiva o componente. 
-  // Cancele la suscripción de Observables y separe los controladores 
-  // de eventos para evitar pérdidas de memoria.
   ngOnDestroy(): void {
-    console.log('ngOnDestroy');
+    console.log('ProdcutComponent - ngOnDestroy');
   }
+
+  increasePrice(): void {
+    this.currentPrice ++;
+  }
+
 }

e7bc3a4d7e10f96672fbe576f2fbceca0a7e84ea
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-02-13 21:19:49 -0600

    11-NestJS-Angular: Adding Sigin and login process, encrypt password, and generate JWT Tokens

--- 11-NestJS-Angular/nest-backend/src/auth/auth.service.ts
+++ 11-NestJS-Angular/nest-backend/src/auth/auth.service.ts
@@ -1,18 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
 
-  constructor(@InjectModel(User.name) private userModel: Model<User> ) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>,
+  private jwtService: JwtService ) {}
 
-  create(createAuthDto: CreateUserDto) : Promise<User> {
-    const user = new this.userModel(createAuthDto);
-    return user.save();
+  async create(createAuthDto: CreateUserDto) : Promise<User> {
+      const { password } = createAuthDto;
+      createAuthDto.password = bcryptjs.hashSync(password, 10);
+      
+      const createdUser = new this.userModel(createAuthDto);
+      
+      await createdUser.save();
+
+      const { password: pass, ...result } = createdUser.toJSON();
+      return result;
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = await this.userModel
+      .findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException(`User with email ${email} not found`);
+    }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is not active');
+    }
+
+    const { password: pass, ...result } = user.toJSON();
+
+
+
+    return {
+      message: 'User logged in successfully',
+      user,
+      "token": this.getJwtToken({ id: user.id })
+    };
   }
 
   findAll() {
@@ -29,5 +69,10 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken(payload: JwtPayload): string {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
---
+++ 11-NestJS-Angular/nest-backend/src/auth/interfaces/jwt-payload.ts
@@ -0,0 +1,5 @@
+export interface JwtPayload {
+    id: string,
+    created_at?: number,
+    expires_at?: number
+}

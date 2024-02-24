e886022a9f52fa0fd4f47c500f94e9b2081d5765
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-02-24 15:58:17 -0600

    12-NewFeatures: Adding Control Flow page and updating doc.

--- 12-NewFeatures/src/app/app.routes.ts
+++ 12-NewFeatures/src/app/app.routes.ts
@@ -57,7 +57,7 @@ export const routes: Routes = [
             },
             {
                 path: '',
-                redirectTo: 'change-detection',
+                redirectTo: 'control-flow',
                 pathMatch: 'full'
             }
         ]
--- 12-NewFeatures/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ 12-NewFeatures/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponentComponent } from '@shared/card-component/card-component.component';
+import { IfComponent } from './controls/if/if.component';
+import { SwitchComponent } from './controls/switch/switch.component';
+import { ForComponent } from './controls/for/for.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [IfComponent, SwitchComponent, ForComponent, CardComponentComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
 export class ControlFlowComponent {
-
 }
